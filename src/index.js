adfbfb54e65ff08fcb9e40a649c43752247a3c8d
yingjieweb <yingjieweb@qq.com>
2023-10-19 10:08:08 +0800

    feat: 🎸 Add route info into local storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const oldBtn = document.querySelector('button[onclick="createRemoteQrcode()"]');
 const textareaElement = document.querySelector("#devtool-qrcode-input");
 
+// Retrieve the textarea value from local storage
+const storedTextareaValue = localStorage.getItem("threshRouteCache");
+if (storedTextareaValue) {
+  textareaElement.value = storedTextareaValue;
+  // Hack: Adjust textarea height by manual -> ./changeStyles.js
+  setTimeout(() => {
+    textareaElement.dispatchEvent(
+      new Event("input", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+  });
+}
+
 const newBtn = document.createElement("button");
 newBtn.textContent = "创建远程调试二维码";
 newBtn.id = "devtool-qrcode-trigger-btn";
@@ -18,3 +33,9 @@ newBtn.addEventListener("click", () => {
 
 oldBtn.parentNode.insertBefore(newBtn, oldBtn.nextSibling);
 oldBtn.style.display = "none";
+
+// Listen textarea's value and update local storage
+textareaElement.addEventListener("input", () => {
+  const updatedTextareaValue = textareaElement.value;
+  localStorage.setItem("threshRouteCache", updatedTextareaValue);
+});
